e3f2805596ca8667d70d54c43bb25fb119b7dacc
Ammara-aziz <139876497+Ammara-aziz@users.noreply.github.com>
2024-01-13 11:39:25 +0500

    Add files via upload

---
+++ class.py
@@ -0,0 +1,52 @@
+print("PAKISTAN")
+print("I am going to crack Generative AI") #2sec
+print("I am going to crack Generative AI")
+print("I am going to crack Generative AI")#4 hours
+
+print("I am going to crack Generative AI") 
+print("I am going to crack Generative AI")
+print("I am going to crack Generative AI") 
+
+name="Muhammmad Umar" 
+print(name)
+print(type(name))
+print(id(name))
+
+name="Muhammmad Umar" 
+#name=500
+print(name) # print
+print(type(name)) #type
+print(id(name)) #physical address
+print([i for i in dir(name) if "__" not in i]) # methods and attributes
+
+#names : str = 1900
+print(name)
+
+#names :list [str]=['a','b', 200]
+#print(names) # print
+#print(type(names)) #type
+#print(id(names)) #physical address
+print([i for i in dir(name) if "__" not in i]) # methods and attributes
+
+#name :list [any]= []
+#print(name) # print
+#print(type(name)) #type
+#print(id(name)) #physical address
+#print([i for i in dir(name) if "__" not in i]) # methods and attributes
+age : str = "amm"
+print(age)
+
+#names : str = 1900
+#print(names)
+
+# assign operators
+
+a : int = 7
+b : int = 9
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
+
+
+
